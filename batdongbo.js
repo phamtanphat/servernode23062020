cccfeb9b8f86a207e0463f0161395da4d3192929
phamtanphat <phatdroid94@gmail.com>
2020-07-30 07:32:34 -0700

    finish bat dong bo

--- batdongbo.js
+++ batdongbo.js
@@ -71,13 +71,25 @@ function chia(a , b){
 // .then(hieu => console.log(hieu))
 // .catch(error => console.log(error))
 
-async function tinhToan(){
-    try {
-        const tong = await cong(5,5)
-        const hieu = await tru(tong , 5)
-        console.log(hieu)
-    } catch (error) {
-        console.log(error)
-    }
-}
-tinhToan()
+// async function tinhToan(){
+//     try {
+//         const tong = await cong(5,5)
+//         const hieu = await tru(tong , 5)
+//         console.log(hieu)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// tinhToan()
+
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 500, 'one');
+  });
+  
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'two');
+});
+Promise.race([promise1, promise2]).then((value) => {
+    console.log(value);
+    // Both resolve, but promise2 is faster
+  });
