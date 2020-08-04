a94f2e75fe9a7e1e9e73bba7457662646d25c74c
phamtanphat <phatdroid94@gmail.com>
2020-08-04 06:09:18 -0700

    get , post

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
+const {json} = require('body-parser');
+const {Word} = require('./word')
+
+app.use(json())
  
 app.get('/word', function (req, res) {
-    const en = req.query.en
-    const vn = req.query.vn
-    res.send({en , vn})
+    Word.find({})
+    .then(words => res.send({success : true , words}).end())
+    .catch(error => res.send({success : false , message : error}).end())
+    
+})
+app.post("/word" , (req , res) => {
+    const {en , vn } = req.body
+    if(en.trim() === '' || vn.trim() === ''){
+        return res.send({success : false , message : "Emty value"}).end()
+    }
+    const newword = new Word({en , vn})
+    newword.save()
+    .then(w => res.send({success : true , word : w}).end())
+    .catch(error => res.send({success : false , message : error}).end())
 })
  
 app.listen(3000)
